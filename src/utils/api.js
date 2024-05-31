853ff6830f1e3ade230928a36cf7f029db840e53
Park Chan Young <cksdud52017@gmail.com>
2024-05-31 23:25:51 +0900

    쇼핑몰 만들기 5~6 작업 완료  + proxy 수정 2

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // 상황따라 주소 다름
 // const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
-// const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
+// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 console.log("LOCAL_BACKEND", LOCAL_BACKEND);
 
 const api = axios.create({
